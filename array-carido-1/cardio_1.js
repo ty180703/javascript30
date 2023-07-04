b0a93b6783da5fc3e911ee03e750c17423c941fc
ty180703 <tyreseedwards03@gmail.com>
2023-07-05 00:40:13 +0100

    javascript30 course
    done

---
+++ array-carido-1/cardio_1.js
@@ -0,0 +1,56 @@
+const inventors = [
+    { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
+    { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
+    { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
+    { first: 'Marie', last: 'Curie', year: 1867, passed: 1934 },
+    { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 },
+    { first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 },
+    { first: 'Max', last: 'Planck', year: 1858, passed: 1947 },
+    { first: 'Katherine', last: 'Blodgett', year: 1898, passed: 1979 },
+    { first: 'Ada', last: 'Lovelace', year: 1815, passed: 1852 },
+    { first: 'Sarah E.', last: 'Goode', year: 1855, passed: 1905 },
+    { first: 'Lise', last: 'Meitner', year: 1878, passed: 1968 },
+    { first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909 }
+  ];
+
+const people = ['Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick', 'Beecher, Henry', 'Beethoven, Ludwig', 'Begin, Menachem', 'Belloc, Hilaire', 'Bellow, Saul', 'Benchley, Robert', 'Benenson, Peter', 'Ben-Gurion, David', 'Benjamin, Walter', 'Benn, Tony', 'Bennington, Chester', 'Benson, Leana', 'Bent, Silas', 'Bentsen, Lloyd', 'Berger, Ric', 'Bergman, Ingmar', 'Berio, Luciano', 'Berle, Milton', 'Berlin, Irving', 'Berne, Eric', 'Bernhard, Sandra', 'Berra, Yogi', 'Berry, Halle', 'Berry, Wendell', 'Bethea, Erin', 'Bevan, Aneurin', 'Bevel, Ken', 'Biden, Joseph', 'Bierce, Ambrose', 'Biko, Steve', 'Billings, Josh', 'Biondo, Frank', 'Birrell, Augustine', 'Black, Elk', 'Blair, Robert', 'Blair, Tony', 'Blake, William'];
+
+  //filter method runs thruogh all elements making changes you want 
+
+const fifteen = inventors.filter(function(inventors) {
+    if (inventors.year <= 1600 && inventors.year >= 1500){
+        return true;
+    }
+});
+
+ //map create new array of things from old one 
+
+const first_last = inventors.map(function(inventors){
+    console.log(`${inventors.first} ${inventors.last}`);
+});
+
+const old_young = inventors.sort(function(a,b) {
+    if (a.year >= b.year){
+        return 1;
+    }else {
+        return -1;
+    }
+});
+
+
+
+// sort by years lived
+const oldest = inventors.sort(function(a,b) {
+    const oldest_1 = a.passed - a.year;
+    const youngest_1 = b.passed - b.year;
+    return oldest_1 > youngest_1 ? -1 : 1;
+});
+
+console.table(oldest);
+
+//sort alphabetically last name 
+const alpha = inventors.sort((lastOne, nextOne) => {
+    const [aLast, aFirst] = lastOne.split(', ');
+    const [bLast, bFirst] = nextOne.split(', ');
+    return aLast > bLast ? 1 : -1;
+})
